026f8517f35420b37718b043578bb56ccb66879d
Lucas <lucas@agtlucas.com>
2016-03-06 19:37:29 -0300

    JS Standard

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+'use strict'
+
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 class celtic extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -26,7 +23,7 @@ class celtic extends Component {
           Cmd+D or shake for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -35,18 +32,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('celtic', () => celtic);
+AppRegistry.registerComponent('celtic', () => celtic)
