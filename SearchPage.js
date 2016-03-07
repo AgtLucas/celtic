93a6e4b9860ccf6d63e9b45bb97fa6f1616a8951
Lucas <lucas@agtlucas.com>
2016-03-07 08:20:18 -0300

    Update SearchPage

--- SearchPage.js
+++ SearchPage.js
@@ -11,6 +11,8 @@ import React, {
   Component
 } from 'react-native'
 
+import SearchResults from './SearchResults'
+
 const urlForQueryAndPage = (key, value, pageNumber) => {
   let data = {
     country: 'uk',
@@ -63,7 +65,11 @@ class SearchPage extends Component {
   _handleResponse (response) {
     this.setState({ isLoading: false, message: '' })
     if (response.application_response_code.substr(0, 1) === '1') {
-      console.log(`Properties found: ${response.listings.length}`)
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      })
     } else {
       this.setState({ message: 'Location not recognized; please try again.' })
     }
